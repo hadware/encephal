7fb74b618b6957afe537c4ba759cfa767e68769c
erwan <wan2@hotmail.fr>
2015-05-31 17:51:08 +0200

    Reorganisation of the code, to have an efficient test module

--- fill_subnet.py
+++ fill_subnet.py
@@ -3,9 +3,6 @@ __author__ = 'lrx'
 from nodes.layer import *
 from nodes.connexion import *
 from nodes.math_function.math_function import *
-from execution.training.labeled_database import *
-from execution.testing.labeled_database import *
-from datasink.datasink import *
 from subnet.subnet import *
 
 """
@@ -77,32 +74,3 @@ class FillSubnet:
         #Create the input and output
         subnet.create_input(dropout1)
         subnet.create_output(output_layer)
-
-"""
-Class to calculate statistics on the performance of a network.
-"""
-class Statistic:
-
-    def __init__(self,learn_db,test_db,encoder):
-        self.learn_db=learn_db
-        self.test_db=test_db
-        self.encoder=encoder
-
-
-    def test(self,n):
-        n.randomize()
-        learning = TrainLabeledDatabase(n, self.learn_db, self.encoder, QuadraticError.differential)
-        testing = TestLabeledDatabase(n, self.test_db, self.encoder)
-        learning.online_learn(10000, 1)
-        res=testing.test(False)
-        return res
-
-
-    def mean(self,n,nb_values):
-        m=0
-        for i in range(nb_values):
-            res = self.test(n)
-            print('value' + str(i) + ':' + str(res))
-            m += res
-        print("The average score value is:")
-        print(m/nb_values)
